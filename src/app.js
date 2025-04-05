162e7ddd80765dda457df8607fa99944ecc7fa5f
942-swapnil <swapnil.navghare942@gmail.com>
2025-04-05 19:39:09 +0530

    add user test routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,33 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Welcome to DevTinder application");
+
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Swapnil" , lastName:"Navghare" , gender: "Male"})
 })
+
+app.post("/user",(req,res)=>{
+    res.send("Data updated successfully in database");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted from the  database");
+})
+
 
 app.use("/test",(req,res)=>{
     res.send("Test page");
 })
+
+app.use("/dev",(req,res)=>{
+    res.send("dev page");
+})
+
+app.use("/",(req,res)=>{
+    res.send("Welcome to DevTinder application");
+})
+
+
 
 app.listen(3000,()=>{
     console.log("Server listening on port 3000....")
