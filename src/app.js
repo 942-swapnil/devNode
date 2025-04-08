84818604cf42916d22734e08e4b0026e82250867
942-swapnil <swapnil.navghare942@gmail.com>
2025-04-08 19:59:47 +0530

    CRUD API Created

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ const express = require("express");
 
 const app = express();
 
+
 const connectDB = require("./config/database.js");
+
+app.use(express.json())
 const User = require('./models/user.js');
 
 
-app.post("/singup",async (req,res)=>{
 
-    const user = new User({
-        firstName: "Sachin",
-        lastName : "Tendulkar",
-        emailId: "sachin.Tendulkar@gamil.com",
-        password : "sachin@123"
-    })
+
+app.post("/singup",async (req,res)=>{
+    // console.log(req.body)
+    const user = new User(req.body)
 
     try{
         await user.save()
@@ -22,6 +22,81 @@ app.post("/singup",async (req,res)=>{
         res.status(400).send("User naot created : "+ err.massage);
     }
 
+})
+
+app.post("/user",async (req,res)=>{
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.find({emailId :userEmail })
+        if(users.length ===0){
+            res.status(404).send("User not found");
+        }else{
+            res.send(users);
+        }
+
+    }catch(err){
+        res.status(400).send("Something went wrong") 
+    }
+})
+
+app.get("/feed", async (req,res)=>{
+    try{
+        const user = await User.find({});
+        res.send(user)
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
+})
+
+
+
+app.post("/getUserById",async (req,res)=>{
+    const userID = req.body.userID;
+    console.log(userID);
+
+    try{
+        const user = await User.findById(userID);
+        if(!user){
+            res.status(404).send("User not found")
+        }else{
+            res.send(user);
+        }
+    }catch(err){
+        res.status(400).send("Something went wrong")
+    }
+})
+
+app.delete("/deleteUserById",async (req,res)=>{
+    const userID = req.body.userID;
+    console.log(userID);
+
+    try{
+        const user = await User.findByIdAndDelete(userID);
+        if(!user){
+            res.status(404).send("User not found")
+        }else{
+            res.send("User deleted successfully");
+        }
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
+})
+
+app.patch("/updateUserById", async (req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        
+        const user = await User.findByIdAndUpdate(userId, data);
+        if(!user){
+            res.status(404).send("User not found");
+        }else{
+            res.send("User updated successfully");
+        }
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
 })
 
 
